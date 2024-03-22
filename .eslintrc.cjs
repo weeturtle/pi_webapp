a2070115e3441e3b79ec86b08ccd5898d869083e
Max London <grahamluk66@gmail.com>
2024-03-22 13:15:01 +0000

    Modified eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
   },
 }
